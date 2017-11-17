a41a7a3fab9a46d39cd91d67ea7112d4d7e77e9c
Benoit PIngris <benoit.pingris@epitech.eu>
2017-11-17 15:41:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 let w = window.innerWidth;
 let bootstrapSize = [0, 0, 0, 0];
 
+const formatBootstrap = ['XS', 'SM', 'MD', 'LG'];
 const _ = el => console.log(el);
 
 const modifyBootstrap = (n) => {
+	if (n == -1) {
+		return;
+	}
 	for (let i = 0; i < 4; i++) {
 		if (i != n) {
 			bootstrapSize[i] = 0;
 		}
 	}
+	bootstrapSize[n] = 1;
+	//Change to this to a real mini popup in the corner
+	_(`Boostrap Breakpoint : ${formatBootstrap[n]}`);
 }
 
 const refreshBoostrapSize = () => {
-	if (w <= 767 && bootstrapSize[0] == 0) {
-		_('Bootstrap BreakPoint : XS');
-		bootstrapSize[0] = 1;
-		modifyBootstrap(0);
-	} else if (w > 767 && w <= 992 && bootstrapSize[1] == 0) {
-		_('Bootstrap BreakPoint : SM');
-		bootstrapSize[1] = 1;
-		modifyBootstrap(1);
-	} else if (w > 992 && w <= 1199 && bootstrapSize[2] == 0) {
-		_('Bootstrap BreakPoint : MD');
-		bootstrapSize[2] = 1;
-		modifyBootstrap(2);
-	} else if (w > 1199 && bootstrapSize[3] == 0) {
-		_('Bootstrap BreakPoint : LG');
-		bootstrapSize[3] = 1;
-		modifyBootstrap(3);
-	}
+	n = (w <= 767 && bootstrapSize[0] == 0) ? 0 :
+		(w > 767 && w <= 992 && bootstrapSize[1] == 0) ? 1 :
+		(w > 992 && w <= 1199 && bootstrapSize[2] == 0) ? 2 :
+		(w > 1199 && bootstrapSize[3] == 0) ? 3 :
+		-1
+	modifyBootstrap(n);
 }
 
 refreshBoostrapSize();
