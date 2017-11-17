08e70fdd919976fb4cf6d9082f28c557a9a93ad3
Benoit PIngris <benoit.pingris@epitech.eu>
2017-11-17 15:50:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,17 +12,16 @@ const modifyBootstrap = (n) => {
 		}
 	}
 	bootstrapSize[n] = 1;
-	//Change this to a real mini popup in the corner
+	//Change to this to a real mini popup in the corner
 	_(`Boostrap Breakpoint : ${formatBootstrap[n]}`);
 }
 
 const refreshBoostrapSize = () => {
-	n = (w <= 767 && bootstrapSize[0] == 0) ? 0 :
+	modifyBootstrap(n = (w <= 767 && bootstrapSize[0] == 0) ? 0 :
 		(w > 767 && w <= 992 && bootstrapSize[1] == 0) ? 1 :
 		(w > 992 && w <= 1199 && bootstrapSize[2] == 0) ? 2 :
 		(w > 1199 && bootstrapSize[3] == 0) ? 3 :
-		-1
-	modifyBootstrap(n);
+		-1)
 }
 
 refreshBoostrapSize();
